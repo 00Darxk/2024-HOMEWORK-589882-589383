db99fc4147b28a0a9770b5bd7015c2c13bf948db
00Darxk <gsturm2003@gmail.com>
2024-05-01 17:29:54 +0200

    Aggiunta StanzaMagica

---
+++ test/it/uniroma3/diadia/ambienti/StanzaMagicaTest.java
@@ -0,0 +1,14 @@
+package it.uniroma3.diadia.ambienti;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StanzaMagicaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
